05e7b46cf047c9ed3dabf25bccdd4ae8f3c902aa
Monster Zero <dinanj@gmail.com>
2021-05-24 11:34:22 -0400

    into the overrides

--- kubejs/startup_scripts/aof_logo.js
+++
@@ -1,6 +0,0 @@
-// Listen to item registry event
-onEvent('item.registry', event => {
-  // The texture for this item has to be placed in kubejs/assets/kubejs/textures/item/test_item.png
-  // If you want a custom item model, you can create one in Blockbench and put it in kubejs/assets/kubejs/models/item/test_item.json
-  event.create('aof_logo').displayName('AOF Logo').type('basic')
-})
