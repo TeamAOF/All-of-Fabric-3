06819e1865873de662591d1094e70b4d6eaabd30
css <88648036+cssmc@users.noreply.github.com>
2022-01-10 03:15:57 +0100

    #9 [10.01.2022]
    additional commit:
    
    - Add qol recipes script;
    - Add egg recipes;
    
    bonus to encourage players to collect items and use them to craft eggs, due to the fact that this is my old script, it does not contain bugs but it certainly contains mod items that your modpack does not have.

--- kubejs/server_scripts/qol.js
+++ kubejs/server_scripts/qol.js
@@ -6,10 +6,9 @@ events.listen("recipes", function (event) {
 
   // Sticks
   event.shaped(item.of("minecraft:stick", 16), [
-    ["#minecraft:logs"],
-    ["#minecraft:logs"],
-  ]);
-
+     ["#minecraft:logs"],
+     ["#minecraft:logs"],
+    ]);
 
   // Hopper
   event.remove({ id: "charm:variant_chests/hopper" });
@@ -18,4 +17,37 @@ events.listen("recipes", function (event) {
       ["minecraft:iron_ingot", "#minecraft:logs",     "minecraft:iron_ingot"],
       [null,                   "minecraft:iron_ingot", null],
     ]);
+	
+  // Buckets
+  event.shaped(item.of("minecraft:bucket", 9), [
+	["null"          , "null"          , "null"          ],
+	["#c:iron_blocks", "null"          , "#c:iron_blocks"],
+	["null"          , "#c:iron_blocks", "null"          ],
+   ]);
+
+  // Chests
+  event.shaped(item.of("minecraft:chest", 4), [
+	["#minecraft:logs_that_burn", "#minecraft:logs_that_burn", "#minecraft:logs_that_burn"],
+	["#minecraft:logs_that_burn", null                       , "#minecraft:logs_that_burn"],
+	["#minecraft:logs_that_burn", "#minecraft:logs_that_burn", "#minecraft:logs_that_burn"],
+   ]);
+
+  // Firework Rockets  
+  event.remove({ output: "minecraft:firework_rocket" });	
+  event.shaped(item.of("minecraft:firework_rocket", 9), [
+     ["minecraft:gunpowder", "minecraft:paper", "null"],
+     ["minecraft:gunpowder", "minecraft:paper", "null"],
+     ["minecraft:gunpowder", "minecraft:paper", "null"],
+   ]);
+ 	
+  event.remove({ output: "minecraft:firework_rocket" });	
+  event.shaped(item.of("minecraft:firework_rocket", 6), [
+     ["minecraft:gunpowder", "minecraft:paper", "null"],
+     ["minecraft:gunpowder", "minecraft:paper", "null"],
+   ]);
+    
+  event.remove({ output: "minecraft:firework_rocket" });	
+  event.shaped(item.of("minecraft:firework_rocket", 3), [
+    ["minecraft:gunpowder", "minecraft:paper", "null"],
+   ]);
 });
