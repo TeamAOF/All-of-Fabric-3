02cd4e34ae6c2204dd515230ee74d4fa90b026ac
css <88648036+cssmc@users.noreply.github.com>
2022-01-10 02:48:07 +0100

    #8 [10.01.2022]
    - Add missing tag + hide lucernia table in rei;
    - Add missing lang files;
    - Add recipe to obtain leather from rotten_flesh;
    
    Additional;
    consider adding animals banners to your entity_banner blacklist. Player should not receive a reward for killing innocent, helpless animals
    
    degenerate

---
+++ kubejs/server_scripts/minecraft/blasting/materials.js
@@ -0,0 +1,9 @@
+events.listen("recipes", function (event) {
+	
+//╔════════════════════════════════════════╗
+//║             ROTTEN FLESH               ║
+//╚════════════════════════════════════════╝
+
+	event.recipes.minecraft.blasting(item.of('minecraft:leather'), 'minecraft:rotten_flesh')
+})
+
